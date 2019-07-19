0eebcc181913ac1615c9b7a0e014b5ba1114d571
east <east_dong@126.com>
2019-07-19 23:00:59 +0800

    增加度量单位原型类，为后期扩展作准备

--- dist/func.js
+++ dist/func.js
@@ -268,7 +268,7 @@ Iso.div=function(obj1,obj2){
 };
 
 // 如果是node环境，进行模块输出
-if(process && process.version){
+if((typeof process!=='undefined') && process.version){
   measureFunc.Iso=Iso;
   module.exports=measureFunc;
 }
